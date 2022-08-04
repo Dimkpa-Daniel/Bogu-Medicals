71374d1cbbf5a101692ba8df1aed519f74e392aa
Dimkpa-Daniel <danieldimkpa1@gmail.com>
2022-08-04 18:43:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function submitForm() {
+  alert("Thank you for choosing Bogu medicals. An email will be sent to you shortly regarding the details of the appointment. Do have a nice day.");
+  window.open("index.html");
+}
